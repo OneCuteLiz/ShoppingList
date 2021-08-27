8bf415885d643d0780a2786b06d37ee94591a8c1
E. Simone <liz.jenerson@gmail.com>
2021-08-26 20:09:47 -0400

    Add a key:value pair to a JSON file

--- app/models/concerns/list.rb
+++ app/models/concerns/list.rb
@@ -1,7 +1,19 @@
+require 'json'
+
 class List
 
     def add(item)
-        
+
+        # read file & convert to hash
+        file = File.read('sample_file.json')
+        list_from_file = JSON.parse(file)
+
+        # update our hash
+        list_from_file[:car] = item
+
+        # update the file w/ new item
+        File.write(file, JSON.dump(list_from_file))
+
     end
 
 
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 class Manipulate_List < ActiveSupport::TestCase
 
-    test "add an item" do
+        test "add an item to a file" do
 
-        list = List.new
-        item = "cherry"
-        
-        assert list.add(item).has_value?(item), "expected #{list.add(item)} to include #{item}"
+            item = "top"
+            list = List.new
+            file = File.read('sample_file.json')
+            list_from_file = JSON.parse(file)
 
-    end
+            list.add(item)
+            
+            assert list_from_file.has_value?(item), "expected #{list_from_file} to include #{item}"
+        end
 
 
-    test "delete an item" do
-        skip
-    end
+        test "delete an item from a file" do
+            skip
+        end
 
 end
